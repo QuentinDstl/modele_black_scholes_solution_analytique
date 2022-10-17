bc07dc1b62a33281abafa25b4a93845b22c97aee
Swordali23 <alireza.atefi@edu.ece.fr>
2022-10-17 15:52:20 +0200

    plot

--- Black_Scholes_call.py
+++ Black_Scholes_call.py
@@ -1,10 +1,15 @@
 from math import log, sqrt, exp
+from numpy import zeros
 from scipy.stats import norm
 
 def Black_Scholes_call(S, K, T, r, sigma):
+    C = zeros(len(S))
+    P = zeros(len(S))
     for i in range(len(S)):
         d1 = (log(S[i]/K) + (r + sigma**2/2)*T)/(sigma*sqrt(T))
         d2 = d1 - sigma*sqrt(T)
-        C = S[i]*norm.cdf(d1) - K*exp(-r*T)*norm.cdf(d2)
-    #P = K*exp(-r*(T-t))*N2 - S*N1
+        N1 = norm.cdf(d1)
+        N2 = norm.cdf(d2)
+        C[i] = S[i]*N1 - K*exp(-r*T)*N2
+        P[i] = K*exp(-r*(T))*N2 - S[i]*N1
     return C
--- execute.py
+++ execute.py
@@ -1,19 +1,19 @@
-from math import log, sqrt, exp
-from numpy import zeros,array,dot,linspace
+from math import log, exp
+from numpy import zeros, linspace
 import matplotlib.pyplot as plt
 from Black_Scholes_call import Black_Scholes_call
-from scipy.stats import norm
 
-def execute(N, K, T, r, sigma, L):
+def execute(N, K, T, r, sigma):
     L = log(3)
     x = zeros(N)
     S = zeros(N)
     x = linspace(-1,1,N)
     for i in range(N):
-        S = K*exp(L*x[i])
-    C = Black_Scholes_call(S, K, T, r, sigma,)
-    plt.plot(S, C)
-    return C
+        S[i] = K*exp(L*x[i])
+    C = Black_Scholes_call(S, K, T, r, sigma)
+    return [S,C]
 
 
-C = execute(200, 100, 1, 0.1, 0.3,)
+[S, C] = execute(200, 100, 1, 0.1, 0.3)
+plt.plot(S, C)
+plt.show()
