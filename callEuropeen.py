cee7660609978adb07cf1ec2547d1e217e6794fa
Swordali23 <alireza.atefi@edu.ece.fr>
2022-10-17 15:01:30 +0200

    Update callEuropeen.py

--- callEuropeen.py
+++ callEuropeen.py
@@ -4,4 +4,5 @@ from scipy.stats import norm
 def callEuropeen(K, T, r, sigma, S0, t):
     d1 = (log(S0/K) + (r + sigma**2/2)*(T-t)) / (sigma*sqrt(T-t))
     d2 = d1 - sigma*sqrt(T-t)
+    N1=
     return K*exp(-r*(T-t))*norm.cdf(-d2) - S0*norm.cdf(-d1)
