e24b7aaceed8b36edf285d2323457686ffbc8836
Swordali23 <alireza.atefi@edu.ece.fr>
2022-10-17 16:40:36 +0200

    sortie 2d

--- Black_Scholes_call.py
+++ Black_Scholes_call.py
@@ -2,14 +2,26 @@ from math import log, sqrt, exp
 from numpy import zeros
 from scipy.stats import norm
 
-def Black_Scholes_call(S, K, T, r, sigma):
+def Black_Scholes_call(S, K, T, r, sigma, t):
     C = zeros(len(S))
     P = zeros(len(S))
     for i in range(len(S)):
-        d1 = (log(S[i]/K) + (r + sigma**2/2)*T)/(sigma*sqrt(T))
-        d2 = d1 - sigma*sqrt(T)
+        d1 = (log(S[i]/K) + (r + 0.5*sigma**2)*(T-t))/(sigma*sqrt(T-t))
+        d2 = d1 - sigma*sqrt(T-t)
         N1 = norm.cdf(d1)
         N2 = norm.cdf(d2)
-        C[i] = S[i]*N1 - K*exp(-r*T)*N2
-        P[i] = K*exp(-r*(T))*N2 - S[i]*N1
+        C[i] = S[i]*N1 - K*exp(-r*(T-t))*N2
+        P[i] = K*exp(-r*(T-t))*(1-N2) - S[i]*(1-N1)
     return C
+
+# def Black_Scholes_call_3d(S, K, T, r, sigma, t):
+#     C = zeros(len(S))
+#     P = zeros(len(S))
+
+#     d1 = (log(S/K) + (r + 0.5*sigma**2)*(T-t))/(sigma*sqrt(T-t))
+#     d2 = d1 - sigma*sqrt(T-t[i])
+#     N1 = norm.cdf(d1)
+#     N2 = norm.cdf(d2)
+#     C = S*N1 - K*exp(-r*(T-t))*N2
+#     P = K*exp(-r*(T-t))*(1-N2) - S*(1-N1)
+#     return C
--- execute.py
+++ execute.py
@@ -1,19 +1,47 @@
 from math import log, exp
-from numpy import zeros, linspace
+from numpy import zeros, linspace, meshgrid
 import matplotlib.pyplot as plt
-from Black_Scholes_call import Black_Scholes_call
+from mpl_toolkits.mplot3d import Axes3D
+from Black_Scholes_call import Black_Scholes_call, Black_Scholes_call_3d
 
+## fonction sortie Call 2D ##
 def execute(N, K, T, r, sigma):
     L = log(3)
     x = zeros(N)
     S = zeros(N)
     x = linspace(-1,1,N)
+    t=0
     for i in range(N):
         S[i] = K*exp(L*x[i])
-    C = Black_Scholes_call(S, K, T, r, sigma)
+    C = Black_Scholes_call(S, K, T, r, sigma, t)
     return [S,C]
 
+## fonction sortie Call 3D ##
+# def execute3d(N, K, T, r, sigma):
+#     L = log(3)
+#     x = zeros(N)
+#     Svals = zeros(N)
+#     tvals = zeros(N)
+#     x = linspace(-1,1,N)
+#     e = 1*exp(-4)
+#     Svals = linspace(e, 3, N)
+#     tvals = linspace(e, T-e, N)
+#     [Smat, tmat] = meshgrid(Svals, tvals)
+#     for i in range(N):
+#         for j in range(N):
+#             C[i][j] = Black_Scholes_call(Smat[j], K, T, r, sigma, tmat[j])
+#     return [Smat, tmat, C]
+
+
+## Sortie 2d ##
 
 [S, C] = execute(200, 100, 1, 0.1, 0.3)
 plt.plot(S, C)
+plt.plot(S, C)
 plt.show()
+
+## Sortie 3d ##
+
+# [Smat, tmat, C] = execute3d(200, 100, 1, 0.1, 0.3)
+# ax = plt.axes(projection='3d')
+# ax.plot_surface(Smat, tmat, C)
