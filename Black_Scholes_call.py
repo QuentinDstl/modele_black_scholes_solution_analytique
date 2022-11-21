cf4e788b820cb67fea22d48cad185d34664ca0ac
Swordali23 <alireza.atefi@edu.ece.fr>
2022-11-21 22:23:48 +0100

    3d

--- Black_Scholes_call.py
+++ Black_Scholes_call.py
@@ -13,15 +13,3 @@ def Black_Scholes_call(S, K, T, r, sigma, t):
         C[i] = S[i]*N1 - K*exp(-r*(T-t))*N2
         P[i] = K*exp(-r*(T-t))*(1-N2) - S[i]*(1-N1)
     return C
-
-# def Black_Scholes_call_3d(S, K, T, r, sigma, t):
-#     C = zeros(len(S))
-#     P = zeros(len(S))
-
-#     d1 = (log(S/K) + (r + 0.5*sigma**2)*(T-t))/(sigma*sqrt(T-t))
-#     d2 = d1 - sigma*sqrt(T-t[i])
-#     N1 = norm.cdf(d1)
-#     N2 = norm.cdf(d2)
-#     C = S*N1 - K*exp(-r*(T-t))*N2
-#     P = K*exp(-r*(T-t))*(1-N2) - S*(1-N1)
-#     return C
--- execute.py
+++ execute.py
@@ -2,7 +2,8 @@ from math import log, exp
 from numpy import zeros, linspace, meshgrid
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from Black_Scholes_call import Black_Scholes_call, Black_Scholes_call_3d
+from Black_Scholes_call import Black_Scholes_call
+
 
 ## fonction sortie Call 2D ##
 def execute(N, K, T, r, sigma):
@@ -17,31 +18,34 @@ def execute(N, K, T, r, sigma):
     return [S,C]
 
 ## fonction sortie Call 3D ##
-# def execute3d(N, K, T, r, sigma):
-#     L = log(3)
-#     x = zeros(N)
-#     Svals = zeros(N)
-#     tvals = zeros(N)
-#     x = linspace(-1,1,N)
-#     e = 1*exp(-4)
-#     Svals = linspace(e, 3, N)
-#     tvals = linspace(e, T-e, N)
-#     [Smat, tmat] = meshgrid(Svals, tvals)
-#     for i in range(N):
-#         for j in range(N):
-#             C[i][j] = Black_Scholes_call(Smat[j], K, T, r, sigma, tmat[j])
-#     return [Smat, tmat, C]
-
+def execute3d(N, K, T, r, sigma):
+    L = log(3)
+    x = zeros(N)
+    Svals = zeros(N)
+    tvals = zeros(N)
+    C0 = zeros(N)
+    C = zeros((N,N))
+    x = linspace(-1,1,N)
+    e = 1*exp(-4)
+    Svals = linspace(e, 3, N)
+    tvals = linspace(e, T-e, N)
+    for i in range(N):
+        C0 = Black_Scholes_call(Svals, K, T, r, sigma, tvals[i])
+        C[i,:] = C0
+    return [Svals, tvals, C]
 
 ## Sortie 2d ##
-
 [S, C] = execute(200, 100, 1, 0.1, 0.3)
 plt.plot(S, C)
 plt.plot(S, C)
 plt.show()
 
-## Sortie 3d ##
+## sortie Call 3D ##
+[Svals, tvals, C] = execute3d(200, 1, 1, 0.1, 0.3)
+S, t = meshgrid(Svals, tvals)
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.plot_surface(S, t, C, rstride=1, cstride=1, cmap='viridis', edgecolor='none')
+plt.show()
 
-# [Smat, tmat, C] = execute3d(200, 100, 1, 0.1, 0.3)
-# ax = plt.axes(projection='3d')
-# ax.plot_surface(Smat, tmat, C)
+
